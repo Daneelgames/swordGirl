f28cae5045754d86b88b606bd591151f589a17ac
Daniil Ermakov <rubberandrobber@gmail.com>
2016-05-13 16:09:38 +0300

    dead angel_1

---
+++ swordGirl/Assets/Scripts/AngelCorpseController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AngelCorpseController : MonoBehaviour {
+
+    [SerializeField]
+    private int pose = 0;
+
+	void Awake () {
+        GetComponentInChildren<Animator>().SetInteger("Pose", pose);
+	}
+}
